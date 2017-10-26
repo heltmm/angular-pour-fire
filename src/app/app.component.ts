bea4e22786dbb6cada14e4a4a47ff6a0922a8c65
Mark Helt <heltmm@gmail.com>
2017-10-26 14:54:19 -0700

    add seeded kegs to rot component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularPour';
+  selectedKeg = null;
+
+  masterKegs: Keg[] = [
+    new Keg('Fresh Squeezed', 'Deschutes', 'Bend OR', 'IPA', 6.4, 60, 16, 6, 1984, 8),
+    new Keg('Delerium Tremens', 'Delerium', 'Belgium', 'Belgian Strong Ale', 8.5, 26, 12, 8, 992, 2),
+    new Keg('Shower Beer', 'Champion', 'Charlotesville, Va', 'Czech Pils', 4.5, 35, 16, 5, 1984, 4),
+    new Keg('Accumulation', 'New Belgium', 'Ft. Collins, CO', 'White IPA', 6.2, 55, 16, 6, 1984, 1)
+  ];
 }
