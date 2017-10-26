4cec3f50894500e842d0699a6e19c943d126aa43
Mark Helt <heltmm@gmail.com>
2017-10-26 16:30:02 -0700

    update keg-display module to inport kegs from kegservice

--- src/app/keg-display/keg-display.component.ts
+++ src/app/keg-display/keg-display.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Keg } from './../keg.model';
 import { Router } from '@angular/router';
+import { KegService} from '../keg.service'
 
 @Component({
   selector: 'app-keg-display',
   templateUrl: './keg-display.component.html',
-  styleUrls: ['./keg-display.component.css']
+  styleUrls: ['./keg-display.component.css'],
+  providers: [KegService]
 })
 export class KegDisplayComponent implements OnInit {
-  selectedKeg = null;
+
   kegs: Keg[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private kegService: KegService) { }
 
   ngOnInit() {
+    this.kegs = this.kegService.getKegs();
   }
+  
   goToDetailPage(clickedKeg: Keg) {
      this.router.navigate(['kegs', clickedKeg.tap]);
    }
--- src/app/keg.service.ts
+++ src/app/keg.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Keg } from './keg.model';
+import { KEGS } from './kegs';
 
 @Injectable()
 export class KegService {
 
   constructor() { }
 
+  getKegs(){
+    return KEGS;
+  }
 }
