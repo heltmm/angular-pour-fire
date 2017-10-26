a20c44660214e0fa53a695479307d2cb6fb9fb17
Mark Helt <heltmm@gmail.com>
2017-10-26 11:47:01 -0700

    rout admin page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { KegDisplayComponent} from './keg-display/keg-display.component'
+import { AdminComponent} from './admin/admin.component'
 
 const appRoutes: Routes = [
   {
     path:'',
     component: KegDisplayComponent
+  },
+  {
+    path:'admin',
+    component: AdminComponent
   }
 ];
 
