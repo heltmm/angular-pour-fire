9b1bd2dfde46b947b8750b7f19592ada79ee058d
Mark Helt <heltmm@gmail.com>
2017-10-26 16:13:17 -0700

    create keg service

--- src/app/keg-display/keg-display.component.ts
+++ src/app/keg-display/keg-display.component.ts
@@ -9,13 +9,7 @@ import { Router } from '@angular/router';
 })
 export class KegDisplayComponent implements OnInit {
   selectedKeg = null;
-
-  kegs: Keg[] = [
-    new Keg('Fresh Squeezed', 'Deschutes', 'Bend OR', 'IPA', 6.4, 60, 16, 6, 1984, 8),
-    new Keg('Delerium Tremens', 'Delerium', 'Belgium', 'Belgian Strong Ale', 8.5, 26, 12, 8, 992, 2),
-    new Keg('Shower Beer', 'Champion', 'Charlotesville, Va', 'Czech Pils', 4.5, 35, 16, 5, 1984, 4),
-    new Keg('Accumulation', 'New Belgium', 'Ft. Collins, CO', 'White IPA', 6.2, 55, 16, 6, 1984, 1)
-  ];
+  kegs: Keg[];
 
   constructor(private router: Router) { }
 
---
+++ src/app/kegs.ts
@@ -0,0 +1,8 @@
+import { Keg } from './keg.model';
+
+export const KEGS: Keg[] = [
+  new Keg('Fresh Squeezed', 'Deschutes', 'Bend OR', 'IPA', 6.4, 60, 16, 6, 1984, 8),
+  new Keg('Delerium Tremens', 'Delerium', 'Belgium', 'Belgian Strong Ale', 8.5, 26, 12, 8, 992, 2),
+  new Keg('Shower Beer', 'Champion', 'Charlotesville, Va', 'Czech Pils', 4.5, 35, 16, 5, 1984, 4),
+  new Keg('Accumulation', 'New Belgium', 'Ft. Collins, CO', 'White IPA', 6.2, 55, 16, 6, 1984, 1)
+];
